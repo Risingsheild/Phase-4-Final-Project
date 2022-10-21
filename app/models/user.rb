3c0004a8a18117ecce1388611bae5b4f216caa32
nickwells <wellsnick11@yahoo.com>
2022-10-21 15:34:50 -0700

    Model associations added with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :reviews
+    has_many :games, through: :reviews
+
+    validates :username, uniqueness: true, presence: true
+    validates :email presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end
