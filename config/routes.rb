cfc1bb0e56e51cb366bb4689fd449006b236a63e
nickwells <wellsnick11@yahoo.com>
2022-10-21 14:36:01 -0700

    Model and Migrate created

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   get '/signup', to: 'users#create'
 
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
