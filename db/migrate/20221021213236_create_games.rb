18d1d4b6183cb0c504e5e0c5178ca84f5b9ac804
nickwells <wellsnick11@yahoo.com>
2022-10-21 14:54:13 -0700

    Added table columns to the migrations

--- db/migrate/20221021213236_create_games.rb
+++ db/migrate/20221021213236_create_games.rb
@@ -1,6 +1,11 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
+      t.string :title
+      t.string :genre
+      t.string :image_url
+
+      t.integer :platform_id
 
       t.timestamps
     end
--- db/migrate/20221021213356_create_platforms.rb
+++ db/migrate/20221021213356_create_platforms.rb
@@ -1,6 +1,8 @@
 class CreatePlatforms < ActiveRecord::Migration[7.0]
   def change
     create_table :platforms do |t|
+      t.string :title 
+      t.string :image_url
 
       t.timestamps
     end
