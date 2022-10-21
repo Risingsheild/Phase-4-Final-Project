a43900cd78fd1592605dbee2c4b2b3aaee9dd90c
nickwells <wellsnick11@yahoo.com>
2022-10-21 14:28:09 -0700

    Routes Set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :reviews
+  resources :platforms, only: [:index, :show]
+  resources :games, only: [:index, :show, :create]
+  resources :users
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get '/me', to: 'users#show'
+  get '/signup', to: 'users#create'
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? 
 end
