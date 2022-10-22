5d034dbf054443dd25331456a98df07e0172c185
nickwells <wellsnick11@yahoo.com>
2022-10-21 17:17:06 -0700

    Games method added to the game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,21 @@
 class GamesController < ApplicationController
+
+    def index
+        render json: Game.all
+    end
+
+    def show 
+        game = Game.find_by(id: params[:id])
+        render json: game 
+    end
+
+    def create
+        game = Game.create(
+            title: params[:title],
+            genre: params[:genre],
+            image_url: params[:image_url]
+        )
+        render json: game, status: :created
+    end
+    
 end
