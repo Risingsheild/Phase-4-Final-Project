eff2410c489e1c0103f48be3b7578d2999b53763
nickwells <wellsnick11@yahoo.com>
2022-10-24 19:22:53 -0700

    Rough Draft of NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,1 +1,15 @@
-import {NavLink} from 'react-dom'
+import {NavLink} from 'react-router-dom'
+
+function NavBar() {
+    
+    return (
+        <div className='navBar'>
+            <NavLink to='/'>Home Page</NavLink>
+            <NavLink to='/pc/games'>PC games</NavLink>
+            <NavLink to='/xbox/games'>Xbox games</NavLink>
+            <NavLink to='/playstation/games'>Playstation games</NavLink>
+        </div>
+    )
+}
+
+export default NavBar
